9e94e0a15ebd6d4af620d4e3c0e2269bc7fea442
Lucy Murfin <lucy_murfin@msn.com>
2018-08-23 18:17:29 +0100

    Fixes from review

--- oop/Car.php
+++ oop/Car.php
@@ -1,10 +1,7 @@
 <?php
 
 class Car extends Vehicles {
-    public function setOwner($owner){
-      
-        parent::setOwner($owner);
-    }
+    
     public function wheels(){
         echo '4 Wheels';
     }
